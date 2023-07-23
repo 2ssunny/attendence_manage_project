69f74707bf8c0643e18f1ff5088fc14c06513cf7
Juho Lee <eungseoni@gmail.com>
2023-07-23 20:01:50 +0900

    start backend with book

--- backend/attendence/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views.about import get_views_about
-from .views.board import insert_board
-
-urlpatterns = [
-    path("about/", get_views_about),
-    path("insertBoard/", insert_board),
-]
--- backend/attendence/utils/board.py
+++
@@ -1,8 +0,0 @@
-# from django.db import models
-from core import models
-
-
-def insert_utils_board():
-    return {
-        "responseCode": True,
-    }
